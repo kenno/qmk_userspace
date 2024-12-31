5c9148e9052499e6e995c154ee29c6f3d201e933
Drashna Jael're <drashna@live.com>
2024-12-31 12:27:30 -0800

    [TractylM] Enable SPI flash for f405

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/rules.mk
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/rules.mk
@@ -33,6 +33,7 @@ ifeq ($(strip $(KEYBOARD)), handwired/tractyl_manuform/5x6_right/f405)
     CUSTOM_QUANTUM_PAINTER_ILI9488        = no
     CUSTOM_QUANTUM_PAINTER_ST7789_170X320 = no
     CUSTOM_QUANTUM_PAINTER_ST7789_135X240 = no
+    WEAR_LEVELING_DRIVER                  = spi_flash
 endif
 
 ifeq ($(strip $(OVERLOAD_FEATURES)), yes)
