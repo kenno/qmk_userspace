767391ed1598edc60cd1519b126312e18d83cfa2
Drashna Jael're <drashna@live.com>
2025-02-21 02:50:39 -0800

    [Display][Menu] Add backlight breathing entry

--- users/drashna/display/menu/submenus/backlight_options.h
+++ users/drashna/display/menu/submenus/backlight_options.h
@@ -35,8 +35,28 @@ __attribute__((weak)) void display_handler_bl_level(char *text_buffer, size_t bu
     snprintf(text_buffer, buffer_len - 1, "%d", get_backlight_level());
 }
 
+#    ifdef BACKLIGHT_BREATHING
+bool menu_handler_breathing(menu_input_t input) {
+    switch (input) {
+        case menu_input_left:
+        case menu_input_right:
+            backlight_toggle_breathing();
+            return false;
+        default:
+            return true;
+    }
+}
+
+__attribute__((weak)) void display_handler_breathing(char *text_buffer, size_t buffer_len) {
+    snprintf(text_buffer, buffer_len - 1, "%s", is_backlight_breathing() ? "on" : "off");
+}
+#    endif
+
 menu_entry_t backlight_entries[] = {
     MENU_ENTRY_CHILD("Backlight Enabled", "Enabled", bl_enabled),
     MENU_ENTRY_CHILD("Backlight Level", "Level", bl_level),
+#    ifdef BACKLIGHT_BREATHING
+    MENU_ENTRY_CHILD("Backlight Breathing", "Breathing", breathing),
+#    endif
 };
 #endif // BACKLIGHT_ENABLE
