f6fba8e366bd425f8c3ddd5e88c65b815fc7fb38
Drashna Jael're <drashna@live.com>
2025-03-26 11:39:58 -0700

    Fix hardware ID code

--- users/drashna/hardware/hardware_id.c
+++ users/drashna/hardware/hardware_id.c
@@ -1,10 +1,19 @@
 // Copyright 2022 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include <ch.h>
-#include <stdint.h>
 #include "hardware_id.h"
+#include <stdlib.h>
 #include <stdio.h>
+#if defined(PROTOCOL_CHIBIOS)
+#    include <ch.h>
+#elif defined(PROTOCOL_LUFA)
+#    ifndef SIGRD
+#        define SIGRD 5
+#    endif // SIGRD
+#    include <avr/boot.h>
+#else
+#    error "How did you get here?"
+#endif
 
 hardware_id_t get_hardware_id(void) {
     hardware_id_t id = {0};
@@ -52,7 +61,7 @@ hardware_id_t get_hardware_id(void) {
 #endif
 
 const char *get_hardware_id_string(void) {
-    static char buf[SERIAL_NUMBER_LENGTH] = {0};
+    static char buf[SERIAL_NUMBER_LENGTH * 2 + 1] = {0};
 #if defined(UID_BASE)
     uint32_t base[3];
     base[0] = (uint32_t)(*((uint32_t *)UID_BASE));
