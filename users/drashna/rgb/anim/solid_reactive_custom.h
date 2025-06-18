b29d323e41f93800b741e72bf963e717036f69b6
Drashna Jael're <drashna@drashna.net>
2025-06-18 15:59:20 -0700

    [RGB Matrix] Add custom solid reactive animation

---
+++ users/drashna/rgb/anim/solid_reactive_custom.h
@@ -0,0 +1,23 @@
+// Copyright 2025 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifdef RGB_MATRIX_KEYREACTIVE_ENABLED
+#    ifdef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+RGB_MATRIX_EFFECT(SOLID_REACTIVE_SIMPLE_CUSTOM)
+#        ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
+
+static hsv_t SOLID_REACTIVE_SIMPLE_math(hsv_t hsv, uint16_t offset) {
+#            ifdef RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
+    hsv.h = scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 8) >> 4);
+#            endif
+    hsv.v = scale8(offset - 1, hsv.v);
+    return hsv;
+}
+
+bool SOLID_REACTIVE_SIMPLE(effect_params_t* params) {
+    return effect_runner_reactive(params, &SOLID_REACTIVE_SIMPLE_math);
+}
+
+#        endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
+#    endif     // ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#endif         // RGB_MATRIX_KEYREACTIVE_ENABLED
